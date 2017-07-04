4e2c097f2fd7493d29cddc626f24146fd2c28e0d
Yoghurt <695498118@qq.com>
2017-07-04 22:53:46 +0800

    test

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -10,6 +10,9 @@
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
     <h2>Ecosystem</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import carousel from '@/components/carousel';
 
 Vue.use(Router);
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/carousel',
+      component: carousel,
     },
   ],
 });
