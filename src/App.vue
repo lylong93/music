ff12ac7e231610c61fa2f61ac3a8f2655de8bea1
Yoghurt <695498118@qq.com>
2017-08-13 23:27:28 +0800

    min

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,22 @@
       </div>
     </div>
     <v-play></v-play>
-    <div class="min-list" v-show="this.minlist">{{this.playlist.length}}</div>
+    <div class="min-list" v-show="this.minlist">
+        <div class="min-list-head">
+          <span class="m-l-h-l m-l-height">播放列表</span>
+          <span class="m-l-h-l">历史记录</span>
+        </div>
+        <div class="min-list-body">
+          <div class="min-list-body-t">
+            <span>总{{this.playlist.length}}首</span>
+            <span>
+              <span>收藏全部</span>
+              <span>清单</span>
+            </span>
+          </div>
+        {{this.playlist}}
+        </div>
+    </div>
     <transition name="fade" enter-active-class="animated bounceInLeft"
     leave-active-class="animated flipOutY"> 
       <v-playshow v-show="this.playshow"></v-playshow>
@@ -201,13 +216,48 @@ export default {
     }
   }
   .min-list{
-    width: 35%;
+    width: 37%;
     height: 70%;
-    background: red;
+    background: rgb(255,255,255);
+    box-shadow: -1px -1px 1px black;
     position: fixed;
     bottom: 70px;
     right: 0px;
     z-index: 90;
+    .min-list-head{
+      display: flex;
+      justify-content:center;
+      height: 50px;
+      width: 100%;
+      background: rgba(147,147,147,.2);
+      font-size: 0;
+      .m-l-h-l{
+        // display: inline-block;
+        font-size:16px;
+        width: 100px;
+        height: 30px;
+        margin:auto 0;
+        line-height: 30px;
+        text-align:center;
+        border:1px solid rgba(147,147,147,.3);
+        background: rgb(255,255,255);
+      }
+      .m-l-height{
+        background: rgb(147,147,147);
+        color:rgb(255,255,255);
+      }
+    }
+    .min-list-body{
+      .min-list-body-t{
+        display: flex;
+        justify-content:space-between;
+        padding: 0 30px;
+        border:1px solid rgba(147,147,147,.3);
+        line-height: 30px;
+        font-size: 16px;
+        
+      }
+    }
   }
 }
 </style>
